a11e95af482c9fda9be8249e610bb8ea1edfee3b
dojdev153 <hitayezufrank@gmail.com>
2025-12-30 11:28:30 +0200

    chargement

--- src/components/common/RouteFallback.tsx
+++ src/components/common/RouteFallback.tsx
@@ -1,7 +1,9 @@
+import { Loader } from "./Loader";
+
 export function RouteFallback() {
   return (
-    <div className="min-h-[40vh] flex items-center justify-center">
-      <div className="animate-pulse text-gray-500">Chargement…</div>
+    <div className="min-h-[60vh] flex items-center justify-center">
+      <Loader />
     </div>
   );
 }
